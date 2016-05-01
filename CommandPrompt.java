54218245e6c9f22c5c1d45d89c62db93704f92b0
Veljko Simic <veljko.simic98@gmail.com>
2016-05-01 20:09:11 +0200

    Create reader

--- CommandPrompt.java
+++ CommandPrompt.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 class CommandPrompt {
     public static void main(String args[]){
-        System.out.println("Hello world");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s = null;
+        while(true){
+            try {
+                System.out.print(">");
+                s = in.readLine();
+                if(s.equals("exit"))
+                    return;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
